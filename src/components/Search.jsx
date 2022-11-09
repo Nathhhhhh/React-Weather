56ac42f16b20687b20671c0442c52a25131f8933
Nathan Harnay <nathan.harnay29@gmail.com>
2022-11-09 16:35:58 +0100

    Ex 9 : Changing places

---
+++ src/components/Search.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+export default function Search({ onSelect }) {
+
+    //state
+    const [city, setCity] = useState('');
+    const [search, setSearch] = useState('');
+    //comportement
+    const handleSelect = (result) => {
+        onSelect(result)
+        setCity('')
+        setSearch('')
+    }
+
+    const getData = () => {
+
+        fetch(`https://geo.api.gouv.fr/communes?nom=${city}&fields=centre,departement&boost=population&limit=5`)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                setSearch(
+                    data.map((result, i) => {
+                        return (
+                            <li key={i}>
+                                <button onClick={() => handleSelect(result)}>{result.nom} ({result.departement.code})</button>
+                            </li>
+                        );
+                    })
+                )
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+    const handleChange = (event) => {
+        const value = event.target.value;
+        setCity(value);
+        getData();
+    }
+
+    //render
+
+    return (
+        <div className="searchbar-container">
+            <div className="searchbar-input-group">
+                <input type="text" className="searchbar-input" placeholder="Rechercher..." value={city} onChange={handleChange} />
+                <button className="searchbar-button" onClick={getData}>
+                    <svg className="searchbar-button-logo" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
+                    </svg>
+                </button>
+            </div>
+            {search !== '' &&
+                <div className="searchbar-options">
+                    <ul>
+                        {search}
+                    </ul>
+                </div>
+            }
+        </div>
+    );
+
+}
---
+++ src/components/WeatherWidget.jsx
@@ -0,0 +1,90 @@
+import ForecastItem from './ForecastItem'
+import TemperatureDisplay from './TemperatureDisplay'
+import WeatherCode from './WeatherCode'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import Proptypes from 'prop-types'
+
+
+export default function WeatherWidget({ city, lat, long }) {
+
+    const baseUrl = 'https://api.open-meteo.com/v1/forecast';
+    const timezone = 'Europe/Paris'
+    const dailyVars = ['weathercode', 'temperature_2m_max', 'temperature_2m_min']
+    const hourlyVars = ['temperature_2m', 'weathercode']
+
+    //state
+    const [weather, setWeather] = useState(null);
+    const [dateNow, setDateNow] = useState(null);
+    const [currentTab, setCurrentTab] = useState('day');
+    //comportement
+    const getData = () => {
+        fetch(`${baseUrl}?latitude=${lat}&longitude=${long}&hourly=${hourlyVars.join(',')}&daily=${dailyVars.join(',')}&timezone=${timezone}&current_weather=true`)
+            .then((response) => response.json())
+            .then((data) => {
+                setDateNow(new Date(Date.now()).toLocaleTimeString('fr'))
+                if (data)
+                    setWeather(data);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+    useEffect(() => {
+        getData();
+        const timer = setInterval(getData, 10000)
+        return () => {
+            clearInterval(timer);
+        }
+
+    }, [])
+    //render
+    return (
+        <div className="weather-container-content">
+            <header className="weather-container-header">
+                <p className="location">{city}</p>
+                <button className="refresh-button" onClick={getData}>
+                    <img src="https://lpmiaw-react.napkid.dev/img/weather/refresh.png" alt="Refresh" />
+                </button>
+            </header>
+            <p className="date">10/20/2021</p>
+            <article className="today">
+
+                {weather !== null
+                    ? <WeatherCode code={weather.current_weather.weathercode} />
+                    : ''
+                }
+                {weather !== null
+                    ? <TemperatureDisplay min={Math.round(weather.daily.temperature_2m_min[0])} max={Math.round(weather.daily.temperature_2m_max[0])} avg={Math.round((weather.daily.temperature_2m_max[0] + weather.daily.temperature_2m_min[0]) / 2)} />
+                    : 'Pas de données'
+                }
+            </article>
+            <section className="">
+                <nav className="tabs">
+                    <button className={`tab ${currentTab === 'day' ? "tab--active" : ''}`} onClick={() => setCurrentTab('day')}>
+                        Journée
+                    </button>
+                    <button className={`tab ${currentTab === 'week' ? "tab--active" : ''}`} onClick={() => setCurrentTab('week')}>
+                        Semaine
+                    </button>
+                </nav>
+                <ul className="forecast">
+                    {currentTab === 'day' && weather !== null &&
+                        new Array(5).fill(null).map((element, index) => <ForecastItem code={weather.hourly.weathercode[(index * 4) + 6]} temperature={(weather.hourly.temperature_2m[(index * 4) + 6])} label={Math.floor(new Date(weather.hourly.time[(index * 4) + 6]).getHours()) + 'h'} key={index} />)
+                    }
+                    {currentTab === 'week' && weather !== null &&
+                        new Array(5).fill(null).map((element, index) => <ForecastItem code={weather.daily.weathercode[index + 1]} temperature={((weather.daily.temperature_2m_max[index + 1] + weather.daily.temperature_2m_min[index + 1]) / 2).toFixed(1)} label={(new Date(weather.daily.time[index + 1]).getDate()) + '/' + new Date(weather.daily.time[index + 1]).getMonth()} key={index} />)
+                    }
+                </ul>
+            </section>
+            <footer className="weather-container-footer">
+                <p>Mis à jour à {dateNow}</p>
+            </footer>
+        </div>
+    );
+
+
+}
+
+WeatherWidget.propTypes = { latitude: Proptypes.number.isRequired, longitude: Proptypes.number.isRequired, cityName: Proptypes.string.isRequired }
